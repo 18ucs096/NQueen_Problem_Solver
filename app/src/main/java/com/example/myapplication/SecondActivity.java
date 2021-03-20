8ca5ec3a3a8527afc6c4c7553eed61aabaca3af0
iamvs-2002 <iamvs2002@gmail.com>
2021-03-21 00:14:26 +0530

    Initial Commit: Implemented dynamic grid layout, giving a Chessboard of NxN as an output.

---
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -0,0 +1,114 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.GridLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    GridLayout gridLayout;
+    RelativeLayout relativeLayout,relativeLayout2;
+    TextView nview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int width = displayMetrics.widthPixels;
+
+
+        relativeLayout = findViewById(R.id.relativelayout);
+        relativeLayout2 = findViewById(R.id.relativelayout2);
+        nview = findViewById(R.id.textn);
+
+        gridLayout = (GridLayout) findViewById(R.id.gridview);
+        gridLayout.removeAllViews();
+
+        Intent x = getIntent();
+        String n = x.getExtras().getString("n");
+
+        nview.setText("Rows = "+n +"\nColumns = "+n);
+
+        int column = Integer.parseInt(n);
+        int row = Integer.parseInt(n);
+
+        gridLayout.setColumnCount(column);
+        gridLayout.setRowCount(row);
+
+        int total = row*column;
+
+        for (int i = 0, c = 0, r = 0; i < total; i++, c++)
+        {
+            if (c == column)
+            {
+                //move to next row after all the columns are filled for first row
+                c = 0;
+                r++;
+            }
+
+            //create a new textview
+            TextView textView = new TextView(this);
+            textView.setId(i);
+            textView.setGravity(Gravity.CENTER);
+            textView.setText(String.valueOf(textView.getId()));
+
+            //setting the background color in the pattern of chess board
+            if (c==r) {
+                textView.setBackgroundColor(Color.parseColor("#000000"));
+                textView.setTextColor(Color.parseColor("#FFFFFF"));
+            }
+            else if(c%2==0 && r%2==0){
+                textView.setBackgroundColor(Color.parseColor("#000000"));
+                textView.setTextColor(Color.parseColor("#FFFFFF"));
+            }
+            else if (c%2!=0 && r%2!=0){
+                textView.setBackgroundColor(Color.parseColor("#000000"));
+                textView.setTextColor(Color.parseColor("#FFFFFF"));
+            }
+            else {
+                textView.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                textView.setTextColor(Color.parseColor("#000000"));
+            }
+
+            //setting the width and height for this textview
+            textView.setWidth((int)width/column
+                    - relativeLayout.getPaddingLeft() - relativeLayout.getPaddingRight()
+                    - relativeLayout2.getPaddingLeft() - relativeLayout2.getPaddingRight());
+            textView.setHeight((int)width/column
+                    - relativeLayout.getPaddingTop() - relativeLayout.getPaddingBottom()
+                    - relativeLayout2.getPaddingTop() - relativeLayout2.getPaddingBottom());
+
+            //setting the row and column number for this textview
+            GridLayout.Spec rowSpan = GridLayout.spec(r, 1);
+            GridLayout.Spec colspan = GridLayout.spec(c, 1);
+
+            GridLayout.LayoutParams gridParam = new GridLayout.LayoutParams(
+                    rowSpan, colspan);
+
+            textView.setLayoutParams(gridParam);
+            gridLayout.addView(textView, gridParam);
+
+        }
+
+        Log.e("Colmn", String.valueOf(gridLayout.getColumnCount()));
+        Log.e("Row", String.valueOf(gridLayout.getRowCount()));
+
+        /*
+        * To access the text view, use:
+        * int id = ID_TO_BE_ACCESSED
+        * textview.findViewById(id)
+        * */
+
+    }
+}
